4f55a8a0df9df5961db8d1417e008d802ca72b98
ttop5 <ttop5@qq.com>
2015-08-02 17:40:44 +0800

    完成基本页面的显示和跳转

--- app/views/index.py
+++ app/views/index.py
@@ -1,7 +1,18 @@
 from app import app
 from flask import render_template
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/ac_life')
+def aclife():
+    return render_template("ac_life.html")
+
+@app.route('/ac_team')
+def acteam():
+    return render_template("ac_team.html")
+
+@app.route('/ac_coach')
+def accoach():
+    return render_template("ac_coach.html")
