2332de078490822d3c32bacb4285d2a1eb4d2bcc
ttop5 <ttop5@qq.com>
2015-08-02 19:04:59 +0800

    更加404异常处理

--- app/views/index.py
+++ app/views/index.py
@@ -6,13 +6,17 @@ def index():
     return render_template("index.html")
 
 @app.route('/ac_life')
-def aclife():
+def ac_life():
     return render_template("ac_life.html")
 
 @app.route('/ac_team')
-def acteam():
+def ac_team():
     return render_template("ac_team.html")
 
 @app.route('/ac_coach')
-def accoach():
+def ac_coach():
     return render_template("ac_coach.html")
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html")
