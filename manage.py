e5af4abf1e46b58341c3d3bf8b35f8222ceeabf9
ttop5 <ttop5@qq.com>
2015-08-16 15:59:06 +0800

    增加创建用户方法python manage.py deploy

--- manage.py
+++ manage.py
@@ -10,12 +10,22 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("runserver", Server(
-    use_debugger=True,
-    use_reloader=True,
-    host="0.0.0.0",
-    port=5000
+        use_debugger=True,
+        use_reloader=True,
+        host="0.0.0.0",
+        port=5000
+    )
 )
-)
+
+@manager.command
+def deploy():
+    user = UserModel.objects(username='admin').first()
+    if user is None:
+        user = UserModel.create_user(
+            username='admin',
+            email='ttop5@qq.com',
+            password='123'
+        ) 
 
 if __name__ == '__main__':
     manager.run()
