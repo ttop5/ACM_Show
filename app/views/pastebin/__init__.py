626376f15b3920a91f58a207c0a642b65339927b
ttop5 <ttop5@qq.com>
2015-08-22 14:52:33 +0800

    增加展示代码页面路由 && 显示页面

--- app/views/pastebin/__init__.py
+++ app/views/pastebin/__init__.py
@@ -1,4 +1,4 @@
-from .pastebin import PastebinView
+from .pastebin import PastebinView, ShareView
 from flask import Blueprint
 
 bp_pastebin = Blueprint('pastebin', __name__)
@@ -6,6 +6,13 @@ bp_pastebin = Blueprint('pastebin', __name__)
 bp_pastebin.add_url_rule(
     '/',
     endpoint='pastebin',
-    view_func=PastebinView.as_view('login'),
+    view_func=PastebinView.as_view('pastebin'),
     methods = ['get', 'post']
+)
+
+bp_pastebin.add_url_rule(
+    '/<int:id>',
+    endpoint='share',
+    view_func=ShareView.as_view('share'),
+    methods = ['get']
 )
--- app/views/pastebin/pastebin.py
+++ app/views/pastebin/pastebin.py
@@ -1,8 +1,20 @@
 from flask import ( # noqa
+    redirect,
+    url_for,
     render_template
 )
 from flask.views import MethodView
 from app.forms import PastebinForm
+from app.models import PastebinModel
+
+
+class ShareView(MethodView):
+
+    template = 'pastebin/share.html'
+
+    def get(self, id):
+        return render_template(self.template)
+
 
 
 class PastebinView(MethodView):
@@ -14,5 +26,10 @@ class PastebinView(MethodView):
         return render_template(self.template, form=form)
 
     def post(self):
-
-        pass
+        form =PastebinForm()
+        p = PastebinModel.objects.create(
+            poster=form.poster.data,
+            syntax=form.syntax.data,
+            content=form.content.data
+        )
+        return redirect(url_for('pastebin.share', id=p.id))
