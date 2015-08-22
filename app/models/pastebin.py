78df480a20f3cc971877a364997bf51349f8b220
ttop5 <ttop5@qq.com>
2015-08-22 15:08:16 +0800

    增加paste的时间字段

--- app/models/pastebin.py
+++ app/models/pastebin.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from app import db
+from datetime import datetime
 
 
 class PastebinModel(db.Document):
     id = db.SequenceField(primary_key=True)
     poster = db.StringField(required=True, max_length=255)
+    time = db.DateTimeField(default=datetime.now())
     syntax = db.StringField(required=True, max_length=255)
     content = db.StringField(required=True)
 
